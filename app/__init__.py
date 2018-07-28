fa91868c477fdd2ec61ec636ce8dc79de3cd937b
hi-the-swan <jason.portenoy@gmail.com>
2018-07-28 16:33:13 -0700

    fix for: CsrfProtect deprecated and renamed to CSRFProtect

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_rq import RQ
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CsrfProtect
+from flask_wtf import CSRFProtect
 # from flask_admin import Admin
 # from flask_admin.contrib.sqla import ModelView
 
@@ -18,7 +18,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 mail = Mail()
 db = SQLAlchemy()
-csrf = CsrfProtect()
+csrf = CSRFProtect()
 compress = Compress()
 # admin = Admin()
 
